199c534154e92534713a271fd85bf2f77b37dc84
robotcigan <robotcigan@gmail.com>
2017-05-23 10:27:11 +0300

    23.05

---
+++ dist/js/main_old.js
@@ -0,0 +1,169 @@
+$(document).ready(function () {
+
+  // wow
+  new WOW().init();
+
+    // Header шапка
+  var page = window.location.pathname;
+
+  if(page === '/wedding/_index.html') {
+    $('.main-header').addClass('main-header--black');
+  }
+
+  $(window).scroll(function () {
+    if( $(window).scrollTop() > 20 ) {
+      $('.main-header').addClass('main-header--scroll');
+    } else {
+      $('.main-header').removeClass('main-header--scroll');
+    }
+  })
+
+
+  // Табы
+  $('.tabs__header .tabs__link').on('click', function() {
+    var index = $(this).index();
+    var parent = $(this).closest('.tabs');
+    parent.find('.tabs__link').removeClass('tabs__link--active');
+    $(this).addClass('tabs__link--active');
+    parent.find('.tabs__content').hide();
+    parent.find('.tabs__content').eq(index).show();
+  });
+
+  $('.accordion .accordion__btn').on('click', function() {
+    var parent = $(this).closest('.accordion');
+    parent.find('.accordion__body').slideDown();
+  })
+
+  $('.show-more').on('click', function() {
+    $(this).text('Скрыть');
+  })
+
+  // Masonry
+  $('.masonry').masonry();
+
+  $('.masonry').isotope({ filter: '*' });
+
+  $('.btn.btn--dashed').eq(0).on('click', function() {
+    $('.masonry').isotope({ filter: '*' });
+  })
+
+  $('.btn.btn--dashed').eq(1).on('click', function() {
+    $('.masonry').isotope({ filter: '.masonry-column__country' });
+  })
+
+  $('.btn.btn--dashed').eq(2).on('click', function() {
+    $('.masonry').isotope({ filter: '.masonry-column__city' });
+  })
+
+
+    // Image gallery
+  $('.portfolio-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    // mainClass: 'mfp-animation',
+    gallery: {
+      enabled: true
+    }
+  });
+
+  $('.documents-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    // mainClass: 'mfp-animation',
+    gallery: {
+      enabled: true
+    }
+  });
+
+  $('.reg-action__container--first').mouseenter(function() {
+    $('.reg-action').css("background-image", "url('../img/registration-specialist.jpg')")
+  })
+  $('.reg-action__container--second').mouseenter(function() {
+    $('.reg-action').css("background-image", "url('../img/registration-user.jpg')")
+  })
+
+
+  // Датапикер
+  $('.datepicker').flatpickr({
+    dateFormat: "d.m.Y",
+    altInput: true
+  });
+  
+  $('.datepicker-inline').flatpickr({
+    inline: true,
+    dateFormat: "d.m.Y",
+    altInput: true,
+    onChange: function(selectedDates, dateStr, instance) {
+      $('.datepicker-inline-date').val(dateStr);
+    }
+  });
+
+    // Кастомный селект
+  $('.custom-select').select2({
+    minimumResultsForSearch: Infinity
+  });
+
+  $('.ya-share2__icon').after('<span class="ya-share2__text">Поделиться</span>');
+
+  $('.ya-share2__link').each(function() {
+    if ( $(this).attr('title') === 'Twitter' ) {
+      $(this).find('.ya-share2__text').text('Твитнуть')
+    }
+  })
+
+  // Слайдеры
+  $('.commercial-slider').slick({
+    dots: false,
+    arrows: true
+  })
+
+  $('.slider').slick({
+    dots: false,
+    infinite: true,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  })
+
+  // Аудио
+  // mejs.i18n.language('ru'); // Setting German language
+    
+  // $('audio,video').mediaelementplayer({
+  //     success: function(player, node) {
+      
+  //       // Optional
+  //         $(player).closest('.mejs__container').attr('lang', mejs.i18n.language());
+          
+  //         $('html').attr('lang', mejs.i18n.language());
+          
+  //         // More code
+  //     }
+  // });
+
+})
--- gulpfile.coffee
+++ gulpfile.coffee
@@ -1,13 +1,14 @@
 gulp = require 'gulp'
 stylus = require 'gulp-stylus'
 nib = require 'nib'
-rupture = require 'rupture'
+# rupture = require 'rupture'
 autoprefixer = require 'gulp-autoprefixer'
 uglifycss = require 'gulp-uglifycss'
 sourcemaps = require 'gulp-sourcemaps'
 # jade = require 'gulp-jade'
 pug = require 'gulp-pug'
 # coffee = require 'gulp-coffee'
+babel = require 'gulp-babel'
 browserSync = require 'browser-sync'
 reload = browserSync.reload
 
@@ -19,17 +20,26 @@ swallowError = (error) ->
 
 gulp.task 'stylus', ->
   gulp.src 'styles/*.styl'
-    # .pipe sourcemaps.init()
+    .pipe sourcemaps.init()
     .pipe stylus
       'include css': true
-      use: [nib(), rupture()]
+      use: [nib()]
       # compress: true
     .on 'error', swallowError
     # .pipe autoprefixer({browsers: ['> 1%', 'last 5 version','IE 10'], cascade: false})
     # .pipe uglifycss
     #   'uglyComments': true
-    # .pipe sourcemaps.write('.')
+    .pipe sourcemaps.write('.')
     .pipe gulp.dest 'dist/styles/css/'
+    .pipe(reload({stream: true, match: '**/*.css'}))
+
+
+gulp.task 'es6', ->
+  gulp.src 'js/main.js'
+    .pipe babel
+      presets: ['es2015']
+    .on 'error', swallowError
+    .pipe gulp.dest 'dist/js'
     .pipe(reload({stream: true}))
 
 
@@ -53,22 +63,25 @@ gulp.task 'pug', ->
 
 # gulp.task('jade-watch', ['jade'], reload)
 gulp.task('pug-watch', ['pug'], reload)
-
-gulp.task 'watch', ->
-  gulp.watch 'styles/**/*.styl', ['stylus']
+# gulp.task 'watch', ->
+  # gulp.watch 'styles/**/*.styl', ['stylus']
+  # gulp.watch 'js/script.js', ['es6']
   # gulp.watch 'js/*.coffee',   ['coffee']
-  gulp.watch '*.pug',        ['pug']
+  # gulp.watch '*.pug',        ['pug']
 
 
 # gulp.task 'default', ['pug', 'stylus', 'coffee','watch'], ->
-gulp.task 'default', ['pug', 'stylus', 'watch'], ->
+gulp.task 'default', ['pug', 'stylus', 'es6'], ->
 
   browserSync
     server: 'dist/'
     notify: false
     open: false
 
+  gulp.watch 'js/main.js', ['es6']
   # gulp.watch('styles/*.styl', ['stylus'])
-  gulp.watch('dist/styles/css/main.css')
+  # gulp.watch('dist/styles/css/main.css')
+  gulp.watch 'styles/**/*.styl', ['stylus']
   # gulp.watch('js/*.coffee',     ['coffee'])
+  # gulp.watch('*.pug',      ['pug-watch'])
   gulp.watch('*.pug',      ['pug-watch'])
